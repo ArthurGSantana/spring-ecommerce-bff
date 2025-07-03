b07009b3f310732abc652e3eeff8e4d201c5fcea
Arthur Santana <arthurgn.santana@gmail.com>
2025-07-03 11:31:48 -0300

    add Spotless plugin for code formatting and clean up main application structure

--- src/main/java/com/ags/spring_ecommerce_bff/SpringEcommerceBffApplication.java
+++ src/main/java/com/ags/spring_ecommerce_bff/SpringEcommerceBffApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringEcommerceBffApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringEcommerceBffApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(SpringEcommerceBffApplication.class, args);
+  }
 }
