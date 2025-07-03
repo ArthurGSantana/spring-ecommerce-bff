6194a0ef4ec5ddf434f3fcd244203dbafddc9ffa
Arthur Santana <arthurgn.santana@gmail.com>
2025-07-03 11:28:54 -0300

    initialize Spring Boot e-commerce backend with Gradle setup and essential configurations

---
+++ src/main/java/com/ags/spring_ecommerce_bff/SpringEcommerceBffApplication.java
@@ -0,0 +1,13 @@
+package com.ags.spring_ecommerce_bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEcommerceBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEcommerceBffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ags/spring_ecommerce_bff/SpringEcommerceBffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ags.spring_ecommerce_bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEcommerceBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
