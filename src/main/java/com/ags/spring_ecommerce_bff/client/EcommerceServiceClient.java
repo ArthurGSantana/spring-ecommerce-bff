22b7b80db1c134b60d8441faae3c8fe1d7a19339
Arthur Santana <arthurgn.santana@gmail.com>
2025-07-08 09:56:05 -0300

    add resilience4j annotations for circuit breaker and retry in EcommerceServiceClient; update error decoder formatting in FeignClientConfig

--- src/main/java/com/ags/spring_ecommerce_bff/client/EcommerceServiceClient.java
+++ src/main/java/com/ags/spring_ecommerce_bff/client/EcommerceServiceClient.java
@@ -2,6 +2,8 @@ package com.ags.spring_ecommerce_bff.client;
 
 import com.ags.spring_ecommerce_bff.config.feign.FeignClientConfig;
 import com.ags.spring_ecommerce_bff.dto.UserDto;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,6 +12,12 @@ import org.springframework.web.bind.annotation.RequestBody;
     name = "ecommerce-service",
     url = "${clients.ecomm-service.url}",
     configuration = FeignClientConfig.class)
+// @CircuitBreaker(name = "ecomm-service", fallbackMethod = "fallbackMethod") // Descomente para
+// usar o Circuit Breaker com fallback
+@CircuitBreaker(name = "ecomm-service")
+@Retry(name = "ecomm-service")
+// @TimeLimiter(name = "ecomm-service") // Deve ser usado apenas em métodos que retornam
+// CompletableFuture ou Mono
 public interface EcommerceServiceClient {
   @PostMapping("/user")
   UserDto createUser(@RequestBody UserDto userDto);
--- src/main/java/com/ags/spring_ecommerce_bff/config/feign/FeignClientConfig.java
+++ src/main/java/com/ags/spring_ecommerce_bff/config/feign/FeignClientConfig.java
@@ -2,9 +2,8 @@ package com.ags.spring_ecommerce_bff.config.feign;
 
 import com.ags.spring_ecommerce_bff.exception.NotFoundException;
 import feign.Client;
-import java.util.concurrent.TimeUnit;
-
 import feign.codec.ErrorDecoder;
+import java.util.concurrent.TimeUnit;
 import okhttp3.OkHttpClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -28,11 +27,12 @@ public class FeignClientConfig {
 
   @Bean
   public ErrorDecoder errorDecoder() {
-    return (methodKey, response) -> switch (response.status()) {
-        case 400, 409 -> new IllegalArgumentException("Invalid request parameters");
-        case 404 -> new NotFoundException("Resource not found");
-        case 500 -> new RuntimeException("Internal server error");
-        default -> new RuntimeException("Unexpected error occurred: " + response.status());
-    };
+    return (methodKey, response) ->
+        switch (response.status()) {
+          case 400, 409 -> new IllegalArgumentException("Invalid request parameters");
+          case 404 -> new NotFoundException("Resource not found");
+          case 500 -> new RuntimeException("Internal server error");
+          default -> new RuntimeException("Unexpected error occurred: " + response.status());
+        };
   }
 }
