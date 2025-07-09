9d88ef50e633ff6850ed4430c2180b93dad5cd93
Arthur Santana <arthurgn.santana@gmail.com>
2025-07-09 10:00:50 -0300

    add security configuration; implement PasswordConfig, SecurityConfig, and TokenService for JWT authentication

--- src/main/java/com/ags/spring_ecommerce_bff/config/security/SecurityConfig.java
+++ src/main/java/com/ags/spring_ecommerce_bff/config/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.ags.spring_ecommerce_bff.config.security;
 
+import com.ags.spring_ecommerce_bff.exception.auth.CustomAccessDeniedHandler;
+import com.ags.spring_ecommerce_bff.exception.auth.CustomAuthenticationEntryPoint;
 import com.ags.spring_ecommerce_bff.filter.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -18,6 +20,8 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @RequiredArgsConstructor
 public class SecurityConfig {
   private final JwtAuthenticationFilter jwtAuthenticationFilter;
+  private final CustomAccessDeniedHandler accessDeniedHandler;
+  private final CustomAuthenticationEntryPoint authenticationEntryPoint;
 
   @Bean()
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -26,12 +30,21 @@ public class SecurityConfig {
             session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(
             auth ->
-                //                auth.requestMatchers("/v3/api-docs/**", "/swagger-ui/**",
-                // "/swagger-ui.html")
-                //                    auth.permitAll() // Swagger
-                auth.anyRequest().permitAll() // Restringe todas as rotas
-            )
-        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                auth.requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html")
+                    .permitAll() // Swagger
+                    .requestMatchers("/api/auth/**")
+                    .permitAll() // Permite acesso público às rotas de autenticação
+                    .anyRequest()
+                    .authenticated())
+        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+        .exceptionHandling(
+            exc ->
+                exc.accessDeniedHandler(
+                        accessDeniedHandler) // lança exceção personalizada quando o acesso é negado
+                    .authenticationEntryPoint(
+                        authenticationEntryPoint) // lança exceção personalizada quando a
+            // autenticação falha
+            );
 
     return http.build();
   }
---
+++ src/main/java/com/ags/spring_ecommerce_bff/service/TokenService.java
@@ -0,0 +1,53 @@
+package com.ags.spring_ecommerce_bff.service;
+
+import com.ags.spring_ecommerce_bff.config.security.JwtConfig;
+import com.ags.spring_ecommerce_bff.entity.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+import javax.crypto.SecretKey;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TokenService {
+  private final JwtConfig jwtConfig;
+
+  public String generateAccessToken(User user) {
+    SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtConfig.getSecret()));
+
+    Date now = new Date();
+    Date expiryDate = new Date(now.getTime() + jwtConfig.getExpirationAccessToken());
+
+    return Jwts.builder()
+        .subject(user.getName())
+        .issuedAt(now)
+        .expiration(expiryDate)
+        .claim("userId", user.getId())
+        .claim("roles", List.of(user.getRole()))
+        .claim("tokenType", "access")
+        .signWith(key)
+        .compact();
+  }
+
+  public String generateRefreshToken(User user) {
+    SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtConfig.getSecret()));
+
+    Date now = new Date();
+    Date expiryDate = new Date(now.getTime() + jwtConfig.getExpirationRefreshToken());
+
+    return Jwts.builder()
+        .subject(user.getName())
+        .issuedAt(now)
+        .expiration(expiryDate)
+        .id(UUID.randomUUID().toString())
+        .claim("userId", user.getId())
+        .claim("tokenType", "refresh")
+        .signWith(key)
+        .compact();
+  }
+}
