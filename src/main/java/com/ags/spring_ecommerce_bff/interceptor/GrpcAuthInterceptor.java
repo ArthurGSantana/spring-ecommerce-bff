2ce05d7cb009930773055dfdc3834be4dc52d935
Arthur Santana <arthurgn.santana@gmail.com>
2025-07-18 18:06:29 -0300

    add GrpcAuthInterceptor to inject authorization token into gRPC client requests

---
+++ src/main/java/com/ags/spring_ecommerce_bff/interceptor/GrpcAuthInterceptor.java
@@ -0,0 +1,25 @@
+package com.ags.spring_ecommerce_bff.interceptor;
+
+import com.ags.spring_ecommerce_bff.service.TokenService;
+import io.grpc.*;
+import lombok.RequiredArgsConstructor;
+import net.devh.boot.grpc.client.interceptor.GrpcGlobalClientInterceptor;
+
+@GrpcGlobalClientInterceptor
+@RequiredArgsConstructor
+public class GrpcAuthInterceptor implements ClientInterceptor {
+  private final TokenService tokenService;
+
+  @Override
+  public <ReqT, RespT> ClientCall<ReqT, RespT> interceptCall(MethodDescriptor<ReqT, RespT> methodDescriptor, CallOptions callOptions, Channel channel) {
+    return new ForwardingClientCall.SimpleForwardingClientCall<>(channel.newCall(methodDescriptor, callOptions)) {
+      @Override
+      public void start(Listener<RespT> responseListener, Metadata headers) {
+        Metadata.Key<String> AUTHORIZATION_KEY =
+            Metadata.Key.of("Authorization", Metadata.ASCII_STRING_MARSHALLER);
+        headers.put(AUTHORIZATION_KEY, tokenService.getCurrentToken());
+        super.start(responseListener, headers);
+      }
+    };
+  }
+}
