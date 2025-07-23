25b1bbed84718e5ada4339b2270b2017e9c45777
Arthur Santana <arthurgn.santana@gmail.com>
2025-07-23 09:28:00 -0300

    add create, update, and delete product mutations to ProductGraphqlController

--- src/main/java/com/ags/spring_ecommerce_bff/graphql/controller/ProductGraphqlController.java
+++ src/main/java/com/ags/spring_ecommerce_bff/graphql/controller/ProductGraphqlController.java
@@ -1,11 +1,14 @@
 package com.ags.spring_ecommerce_bff.graphql.controller;
 
+import com.ags.spring_ecommerce_bff.dto.request.ProductRequestDto;
 import com.ags.spring_ecommerce_bff.dto.response.ProductResponseDto;
 import com.ags.spring_ecommerce_bff.service.ProductService;
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
@@ -24,5 +27,45 @@ public class ProductGraphqlController {
   @QueryMapping
   public List<ProductResponseDto> getAllProducts() {
     return productService.getAllProducts();
+  }
+
+  @MutationMapping
+  public ProductResponseDto createProduct(
+      @Argument String name,
+      @Argument String description,
+      @Argument Double price,
+      @Argument Integer stock) {
+    var product =
+        ProductRequestDto.builder()
+            .name(name)
+            .description(description)
+            .price(BigDecimal.valueOf(price))
+            .stock(stock)
+            .build();
+    return productService.createProduct(product);
+  }
+
+  @MutationMapping
+  public ProductResponseDto updateProduct(
+      @Argument UUID id,
+      @Argument String name,
+      @Argument String description,
+      @Argument Double price,
+      @Argument Integer stock) {
+    var product =
+        ProductRequestDto.builder()
+            .id(id)
+            .name(name)
+            .description(description)
+            .price(BigDecimal.valueOf(price))
+            .stock(stock)
+            .build();
+    return productService.updateProduct(id, product);
+  }
+
+  @MutationMapping
+  public Boolean deleteProduct(@Argument UUID id) {
+    productService.deleteProduct(id);
+    return true;
   }
 }
