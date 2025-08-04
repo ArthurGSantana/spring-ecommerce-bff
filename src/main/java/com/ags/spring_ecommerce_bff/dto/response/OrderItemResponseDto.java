0dc82841a9e980ff1f39cfb445e78638a7576112
Arthur Santana <arthurgn.santana@gmail.com>
2025-08-04 09:45:47 -0300

    add constructors to OrderItemResponseDto and OrderResponseDto for improved object initialization

--- src/main/java/com/ags/spring_ecommerce_bff/dto/response/OrderItemResponseDto.java
+++ src/main/java/com/ags/spring_ecommerce_bff/dto/response/OrderItemResponseDto.java
@@ -1,11 +1,16 @@
 package com.ags.spring_ecommerce_bff.dto.response;
 
 import java.util.UUID;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderItemResponseDto {
   private UUID productId;
   private Integer quantity;
--- src/main/java/com/ags/spring_ecommerce_bff/dto/response/OrderResponseDto.java
+++ src/main/java/com/ags/spring_ecommerce_bff/dto/response/OrderResponseDto.java
@@ -2,11 +2,16 @@ package com.ags.spring_ecommerce_bff.dto.response;
 
 import java.util.List;
 import java.util.UUID;
+
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderResponseDto {
   private UUID id;
   private UUID shippingAddressId;
